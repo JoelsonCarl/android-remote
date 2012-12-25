fab42c1fa81a8c8250848238d862cfda1b805812
Joel Carlson <JoelsonCarl@gmail.com>
2012-12-25 00:55:12 -0600

    Rough Start of UI
    Starts work on the UI. Adds volume control,
    mouse control, and connection settings tabs.
    Volume control and mouse control both contain
    non-functional elements.

--- AndroidRemote/src/com/joelsoncarl/androidremote/MainActivity.java
+++ AndroidRemote/src/com/joelsoncarl/androidremote/MainActivity.java
@@ -1,7 +1,9 @@
 package com.joelsoncarl.androidremote;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,7 +11,43 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main_volume);
+		
+		final ActionBar actionBar = getActionBar();
+
+	    // Specify that tabs should be displayed in the action bar.
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+	    // Create a tab listener that is called when the user changes tabs.
+	    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+	        public void onTabSelected(ActionBar.Tab tab,
+	                FragmentTransaction ft) {
+	        	if (tab.getText().toString().compareTo(
+	        			getResources().getString(R.string.volume_tab_name)) == 0) {
+	        		setContentView(R.layout.activity_main_volume);
+	        	}
+	        	else if (tab.getText().toString().compareTo(
+	        			getResources().getString(R.string.mouse_tab_name)) == 0) {
+	        		setContentView(R.layout.activity_main_mouse);
+	        	}
+	        	else if (tab.getText().toString().compareTo(
+	        			getResources().getString(R.string.connect_tab_name)) == 0) {
+	        		setContentView(R.layout.activity_main_connect);
+	        	}
+	        }
+
+	        public void onTabUnselected(ActionBar.Tab tab,
+	                FragmentTransaction ft) { }
+
+	        public void onTabReselected(ActionBar.Tab tab,
+	                FragmentTransaction ft) { }
+	    };
+
+	    // Add tabs
+	    actionBar.addTab(actionBar.newTab().setText("Volume").setTabListener(tabListener));
+	    actionBar.addTab(actionBar.newTab().setText("Mouse").setTabListener(tabListener));
+	    actionBar.addTab(actionBar.newTab().setText("Connect").setTabListener(tabListener));
+
 	}
 
 	@Override
